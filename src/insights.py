87a292892ded430d438f64e3cfb67eb3bb1607fa
Jacqueline Silva <jacque.sx@hotmail.com>
2022-10-20 15:33:12 -0300

    :sparkles: feat: implementa função 'get_unique_job_types'
    A função recebe um parametro com o caminho para o arquivo CSV, usa-o na função criada anteriormente: 'jobs.read' que retorna lista de dicionários. A partir dai é feito um filtro para retornar o campo 'job_type' únicos.

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,11 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    """Checks all different job types and returns a list of them"""
+    list_jobs = read(path)
+    list_unique_job_types = {row["job_type"] for row in list_jobs}
+    return list_unique_job_types
 
 
 def filter_by_job_type(jobs, job_type):
