77235e02e0c660e22c4eb763c06faa29934a0980
Jacqueline Silva <jacque.sx@hotmail.com>
2022-10-20 15:00:18 -0300

    :sparkles: feat: implementa função 'read'
    Função recebe uma str com o caminho para um arquivo CSV, abre-o e retorna o conteúdo no formato de uma lista de dicionários.

--- src/jobs.py
+++ src/jobs.py
@@ -1,21 +1,11 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
-
-
-# commit inicial
+    """Reads a file from a given path and returns its contents"""
+    with open(path) as file:
+        reader_file = csv.DictReader(file)
+        list = [row for row in reader_file]
+        return list
