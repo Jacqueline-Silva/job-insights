9814a8ba63861fb92d456728a4bd3e4419136ea4
Jacqueline Silva <jacque.sx@hotmail.com>
2022-10-21 11:38:49 -0300

    :test_tube: test: implementa teste para a função 'read_brazilian_file'

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    verify_keys_incorrects = ["titulo", "salario", "tipo"]
+    verify_keys_corrects = ["title", "salary", "type"]
+    list_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in list_jobs:
+        for key_correct in verify_keys_corrects:
+            assert key_correct in job
+
+        for key_incorrect in verify_keys_incorrects:
+            assert key_incorrect not in job
